20af5bcbc6453082e184f73d816307e64dcdb099
hendelvar <133061825+hendelvar@users.noreply.github.com>
2023-05-11 17:47:09 -0400

    Add files via upload

---
+++ Ejercicio_2/ejercicio_2.js
@@ -0,0 +1,19 @@
+const nombre = "Henry";
+console.log(nombre);
+
+const edad = 33;
+console.log(edad);
+
+const eresDesarrollador = true;
+console.log(eresDesarrollador);
+
+const nacimiento = new Date("april 4 1990");
+console.log(nacimiento);
+
+const libro = {
+    titulo: "It",
+    autor: "Stephen King",
+    fecha: 1986,
+    url: "https://www.amazon.es/BEST-SELLER-Stephen-King/dp/8497593790"
+};
+console.log(libro);
