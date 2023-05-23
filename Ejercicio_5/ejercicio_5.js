c45f1cdd52b050fc898fb4767ded31b81266cb00
hendelvar <133061825+hendelvar@users.noreply.github.com>
2023-05-23 17:14:07 -0400

    Add files via upload

---
+++ Ejercicio_5/ejercicio_5.js
@@ -0,0 +1,11 @@
+let estatura = 168;
+
+let altura_m = 1.68;
+
+let peso = 65.82;
+
+let peso_red = peso.toFixed(1);
+
+let altura_m_red = altura_m.toPrecision(5);
+
+let x = Number.MAX_VALUE + 1 === Number.MAX_VALUE;
