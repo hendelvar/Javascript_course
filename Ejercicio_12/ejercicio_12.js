c8f4b9776bbf3ccb08d51e93b291dd82a9e4c934
hendelvar <133061825+hendelvar@users.noreply.github.com>
2023-06-22 16:40:29 -0400

    Add files via upload

---
+++ Ejercicio_12/ejercicio_12.js
@@ -0,0 +1,15 @@
+function fibo(num) {
+    if(num === 1) 
+    return [1]
+    
+    if(num === 2) 
+    return [1, 1]
+    
+    let lista = [1, 1]
+    for (let i = 2; i < num; i++) {
+        lista.push(lista[i - 1] + lista[i - 2])
+    }
+    return lista
+}
+
+console.log(fibo(12))
